49431a83839826f61607de38c4432be1381198d9
Tom Maitland <tommaitland@me.com>
2013-05-10 02:37:25 +1000

    Functioning carousel, needs transitions

--- html/js/plugins/carousel.js
+++ html/js/plugins/carousel.js
@@ -7,17 +7,65 @@
     var defaults = {
         slide: 'img',
         transition: 'fade',
-        speed: 5000
+        speed: 5000,
+        animationSpeed: 500
     };
-
     var options = $.extend(defaults, options);
 
+    //styles
+    var active = {
+        position: 'relative',
+        'z-index': 1,
+        opacity:1
+    }
+    var inactive = {
+        position: 'absolute',
+        'z-index': 0,
+        opacity:0,
+        top:0
+    }
+
+    
     return this.each(function() {
 
-        //basic dom setup
-        element.find(options.slide).addClass('slide');
+        //basic dom setup + styling
+        element.css({
+            position: 'relative',
+        });
+        element.find(options.slide).addClass('slide').css(inactive);
+
+        //get the first slide
+        element.find(options.slide+':first').addClass('active first').css(active);
+
+        //swap slides
+        var swap = function() {
+            var next = element.find('.active').next(options.slide);
+            
+            //loop through slides
+            if (!next.length > 0)
+                next = element.find('.first');
+
+            element.find('.active').removeClass('active').css(inactive);
+            next.addClass('active').css(active);
+        }
+
+        //run swap function
+        run = setInterval(function(){
+            swap();
+        }, options.speed);
+
+        //pause on hover
+        element.hover(function(){
+            clearInterval(run);
+        }, function() {
+            run = setInterval(function(){
+                swap();
+            }, options.speed);
+        });
+
 
     });
+
   };
 
 })(jQuery);
