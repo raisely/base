ad12e86ce281d923fc8e2fc57a86096a6dc2293d
Tom Maitland <tommaitland@me.com>
2013-05-27 20:46:10 +1000

    WIP - Namespaced, extendable, api-ed carousel plugin

--- html/js/plugins/carousel.js
+++ html/js/plugins/carousel.js
@@ -1,87 +1,134 @@
-(function ($) {
+// Base Carousel
+// A basic carousel script
 
-    $.fn.carousel = function(options) {
+(function($) {
 
-        //variables
-        var $element = $(this);
+    $.carousel = function(element, options) {
+
         var defaults = {
             transition: 'fade',
-            speed: 5000,
+            speed: 3000,
             pauseOnHover: true,
             slide: 'img',
-            slideClass: 'slide',            
-            activeClass: 'active',
-            lastClass: 'last',
-            nextClass: 'next'
+        }
 
-        };
-        var options = $.extend(defaults, options);
+        var plugin = this;
 
-        var actions = {
+        // external access: element.data('carousel').settings.propertyName
+        plugin.settings = {}
 
+        var $element = $(element),
+             element = element;
+
+        plugin.init = function() {
+
+            plugin.settings = $.extend({}, defaults, options);
+
+            plugin.create();
 
         }
-        
-        return this.each( function() {
 
+        // add the basic css classes
+        plugin.create = function () {
 
-            //css config
-            $element.addClass('transition-' + options.transition);
-            $element.find(options.slide).addClass(options.slideClass);
-            
-            //get the first slide
-            var $firstSlide = $element.find(options.slide + ':first');
-            $firstSlide.addClass(options.activeClass);
+          // basic css structure
+          $element.addClass('transition-' + plugin.settings.transition); // transition class
+          $element.find(plugin.settings.slide).addClass('slide'); // slide class
 
-            //set the board
-            $element.find(options.slide + ':last').addClass(options.lastClass);
-            $firstSlide.next(options.slide).addClass(options.nextClass);
+          // set first slide
+          plugin.slide(1, true);
+          plugin.run();
 
-            //swap slides
-            var step = 0;
-            var swap = function() {
-                step = step + 1;
+        },
 
-                var $next = $element.find('.' + options.activeClass).next(options.slide);                
+        // take slider to a specific slide number
+        plugin.slide = function (e,d) {
 
-                //loop through slides
-                if ($next.length == 0) {
-                    $next = $firstSlide;
-                    step = 0;
-                }
+          var i = $element.find(plugin.settings.slide + '.active').index; // get current position    
+          var $new = $element.find(plugin.settings.slide + ':nth-child(' + e + ')');
 
-                //add active class
-                $element.find('.' + options.lastClass).removeClass(options.lastClass);
-                $element.find('.' + options.nextClass).removeClass(options.nextClass);
-                $element.find('.' + options.activeClass).removeClass(options.activeClass).addClass(options.lastClass);
-                $next.addClass(options.activeClass);
-                
-                if ($next.next().length == 0) {
-                     $firstSlide.addClass(options.nextClass);
-                } else {
-                    $next.next().addClass(options.nextClass); 
-                }
+          // get prev slide
+          if ($new.prev('.slide').length == 0) var $prev = $element.find('.slide:last');
+          else var $prev = $new.prev('.slide');
 
-            }
+          // get next slide
+          if ($new.next('.slide').length == 0) var $next = $element.find('.slide:first');
+          else var $next = $new.next('.slide');
 
-            //run swap function
-            run = setInterval(function(){
-                swap();
-            }, options.speed);
+          if (d == null) var d = (i < $new.index()); // get direction
 
-            //pause on hover
-            if (options.pauseOnHover) {
-                $element.hover(function(){
-                    clearInterval(run);
-                }, function() {
-                    run = setInterval(function(){
-                        swap();
-                    }, options.speed);
-                });
+          // change classes
+          $element.find('.slide').removeClass('left active right');
+          $new.addClass('active');
+
+          if (d) {
+            $prev.addClass('left');
+            $next.addClass('right');
+          } else {
+            $prev.addClass('right');
+            $next.addClass('left');
+          }
+
+        },
+
+        // call next slide from current slide
+        plugin.next = function () {
+          
+          var next = $element.find('.active').next('.slide').length;
+          var e = (next == 0) ? 0 : $element.find('.active').index() + 1;
+
+          plugin.slide((e + 1), true);
+
+        },
+
+        // call previous slide from current slide
+        plugin.prev = function () {
+
+          var prev = $element.find('.active').prev('.slide').length;
+          var e = (prev == 0) ? $element.find('.slide:last').index() + 1 : $element.find('.active').index();
+
+          plugin.slide(e, false);
+
+        },
+
+        plugin.run = function () {
+
+          // run carousel
+          setInterval( function () {
+            plugin.next();
+          }, plugin.settings.speed);
+
+        },
+
+        plugin.pause = function () {
+
+        }
+
+        var private = function() {
+
+        }
+
+        plugin.init();
+
+    }
+
+    // add to the jQuery object
+    $.fn.carousel = function(options) {
+
+        return this.each(function() {
+
+            if (undefined == $(this).data('carousel')) {
+
+                var plugin = new $.carousel(this, options);
+
+                // element.data('carousel').publicMethod(arg1, arg2, ... argn)
+                // element.data('carousel').settings.propertyName
+                $(this).data('carousel', plugin);
+
             }
 
         });
 
-    };
+    }
 
 })(jQuery);
