1a5b2088e89ba0415ac2ec6af6d39db2c0aa85a2
Tom Maitland <tommaitland@me.com>
2014-03-10 11:42:15 +1100

    Default grunt config compiles to single file for site optimisation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,22 @@
 module.exports = function (grunt) {
     grunt.initConfig({
 
-        // compress compiled files
-        cssmin: {
-            "css/base.css": ["css/base.css"],
-            "css/desktop.css": ["css/desktop.css"],
-            "css/tablet.css": ["css/tablet.css"],
-            "css/mobile.css": ["css/mobile.css"]
-        },
-
         // watch changes to less files
         watch: {
             styles: {
                 files: ["less/*"],
-                tasks: ["less", "cssmin"]
+                tasks: ["less"]
             }
         },
 
         // compile set less files
         less: {
-            compile: {
+            development: {
                 options: {
-                    paths: ["less"]
+                    paths: ["less"],
                 },
                 files: {
-                    "css/base.css": "less/base.less",
-                    "css/desktop.css": "less/desktop.less",
-                    "css/tablet.css": "less/tablet.less",
-                    "css/mobile.css": "less/mobile.less"
+                    "css/site.css": ["less/*.less", "!less/_*.less"]
                 }
             }
         },
@@ -36,7 +25,6 @@ module.exports = function (grunt) {
 
     // Load tasks so we can use them
     grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
     grunt.loadNpmTasks("grunt-contrib-less");
 
     // the default task will show the usage
@@ -49,5 +37,5 @@ module.exports = function (grunt) {
         grunt.log.writeln("* run 'grunt dev' to start watching and compiling LESS changes.");
     });
 
-    grunt.registerTask("dev", ["less:compile", "watch:styles"]);
+    grunt.registerTask("dev", ["less:development", "watch:styles"]);
 };
