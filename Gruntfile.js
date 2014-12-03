f039f2322d753f3dab89a37e912a4f2aac1cba85
Tom Maitland <tommaitland@me.com>
2014-12-03 11:03:21 +1100

    Switch to BrowserSync, add css sourcemaps

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,3 +52,63 @@ module.exports = function (grunt) {
 
     grunt.registerTask("dev", ["less:development", "styleinjector", "watch:styles"]);
 };
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        // live browser injection
+        browserSync: {
+            bsFiles: {
+                src : 'css/site.css'
+            },
+            options: {
+                watchTask: true
+            }
+        },
+
+        // watch changes to less files
+        watch: {
+            styles: {
+                files: ["less/**/*"],
+                tasks: ["less"]
+            },
+            options: {
+                spawn: false,
+            },
+        },
+
+        // compile set less files
+        less: {
+            development: {
+                options: {
+                    paths: ["less"],
+                    sourceMap: true,
+                    sourceMapFilename: 'css/site.css.map',
+                    sourceMapURL: 'css/site.css.map',
+                    compress: true
+                },
+                files: {
+                    "css/site.css": ["less/*.less", "!less/_*.less"]
+                }
+            }
+        }
+
+    });
+
+    // Load tasks so we can use them
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // the default task will show the usage
+    grunt.registerTask("default", "Prints usage", function () {
+        grunt.log.writeln("");
+        grunt.log.writeln("Building Base");
+        grunt.log.writeln("------------------------");
+        grunt.log.writeln("");
+        grunt.log.writeln("* run 'grunt --help' to get an overview of all commands.");
+        grunt.log.writeln("* run 'grunt dev' to start watching and compiling LESS changes for development.");
+    });
+
+    grunt.registerTask("dev", ["less", "browserSync", "watch"]);
+
+};
