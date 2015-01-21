5727619fa2c22801587a4c77a88f60cdad322646
ryenjbeatty <ryen.beatty@gmail.com>
2015-01-21 14:40:07 +1100

    Merge in pull request #26

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,84 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
 
-        // live browser injection
-        browserSync: {
-            bsFiles: {
-                src : 'css/site.css'
-            },
-            options: {
-                watchTask: true
-            }
-        },
+	grunt.initConfig({
 
-        // watch changes to less files
-        watch: {
-            styles: {
-                files: ["less/**/*"],
-                tasks: ["less"]
-            },
-            options: {
-                spawn: false,
-            },
-        },
+	// Live browser injection
+	browserSync: {
+		bsFiles: {
+			src : ["css/site.css", "js/*.js"]
+		}
+	},
 
-        // compile set less files
-        less: {
-            development: {
-                options: {
-                    paths: ["less"],
-                    sourceMap: true,
-                    sourceMapFilename: 'css/site.css.map',
-                    sourceMapURL: 'site.css.map',
-                    compress: true
-                },
-                files: {
-                    "css/site.css": ["less/*.less", "!less/_*.less"]
-                }
-            }
-        }
+	// Watch changes to files
+	watch: {
+		styles: {
+			files: ["less/**/*.less", "js/**/*.js"],
+			tasks: ["less", "minified"]
+		},
+		options: {
+			spawn: false,
+		},
+	},
 
-    });
+	// Compile set less files
+	less: {
+		development: {
+			options: {
+				paths: ["less"],
+				sourceMap: true,
+				sourceMapFilename: "css/site.css.map",
+				sourceMapURL: "css/site.css.map"
+			},
+			files: {
+				"css/site.css": ["less/*.less", "!less/_*.less"]
+			}
+		},
+		production: {
+			options: {
+				paths: ["less"],
+				sourceMap: false,
+				cssmin: true
+			},
+			files: {
+				"css/site.css": ["less/*.less", "!less/_*.less"]
+			}
+		}
+	},
 
-    // Load tasks so we can use them
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-less");
-    grunt.loadNpmTasks('grunt-browser-sync');
+	// Compress .js files
+	minified : {
+		files: {
+			src: [ 'js/*.js', '!js/*.min.js', '!js/vendor/**/*.js', '!js/**/_*.js'],
+			dest: 'js/'
+		},
+		options : {
+			allinone: false,
+			ext: '.min.js'
+		}
+	}
 
-    // the default task will show the usage
-    grunt.registerTask("default", "Prints usage", function () {
-        grunt.log.writeln("");
-        grunt.log.writeln("Building Base");
-        grunt.log.writeln("------------------------");
-        grunt.log.writeln("");
-        grunt.log.writeln("* run 'grunt --help' to get an overview of all commands.");
-        grunt.log.writeln("* run 'grunt dev' to start watching and compiling LESS changes for development.");
-    });
+	});
 
-    grunt.registerTask("dev", ["less", "browserSync", "watch"]);
+	// Load modules
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-browser-sync");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks('grunt-minified');
+
+	// The default task prints usage
+	grunt.registerTask("default", "Prints usage", function () {
+		grunt.log.writeln("");
+		grunt.log.writeln("Building Base");
+		grunt.log.writeln("------------------------");
+		grunt.log.writeln("");
+		grunt.log.writeln("* run 'grunt --help' to get an overview of all commands.");
+		grunt.log.writeln("* run 'grunt dev' to start watching and compiling LESS changes for development.");
+		grunt.log.writeln("* run 'grunt prod' to minify css and js files for production.");
+	});
+
+	// Create our tasks
+	grunt.registerTask("dev", ["less:development", "minified", "watch", "browserSync"]);
+	grunt.registerTask("prod", ["minified", "less:production"]);
 
 };
